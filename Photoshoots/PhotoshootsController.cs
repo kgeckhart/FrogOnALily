48ab2d640d4e65ccfb237378cc5710c707f61c0e
kgeckhart <kgeckhart@outlook.com>
2017-11-05 12:54:44 -0500

    Adding an api client and adding swagger response annotations

--- Photoshoots/PhotoshootsController.cs
+++ Photoshoots/PhotoshootsController.cs
@@ -3,6 +3,8 @@ using MediatR;
 using System.Threading.Tasks;
 using FrogOnALily.Photoshoots.Model;
 using FrogOnALily.Photoshoots.Query;
+using NSwag.Annotations;
+using System.Collections.Generic;
 
 namespace FrogOnALily.Photoshoots
 {
@@ -17,12 +19,14 @@ namespace FrogOnALily.Photoshoots
         }
 
         [HttpGet("")]
+        [SwaggerResponse(typeof(IEnumerable<Photoshoot>))]
         public async Task<IActionResult> GetPhotoshoots(PhotoshootCategory? category = null)
         {
             return Ok(await _mediator.Send(new PhotoshootsByCategoryQuery(category)));
         }
 
         [HttpGet("{photoshootId}/images")]
+        [SwaggerResponse(typeof(IEnumerable<PhotoshootImage>))]
         public async Task<IActionResult> GetImagesForPhotoshoots(int photoshootId)
         {
             return Ok(await _mediator.Send(new ImagesByPhotoshootIdQuery(photoshootId)));
