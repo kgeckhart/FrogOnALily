959316762e0e7efccbc6b6bc99a31ec9b1107486
kgeckhart <kgeckhart@gmail.com>
2018-02-18 19:44:20 -0500

    Moved website to a web folder Created a new project for the phothsoot upload

---
+++ FrogOnALily.PhotoshootUpload/FrogOnALily.PhotoshootUpload/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FrogOnALily.PhotoshootUpload
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sourceFolder = @"c:\images\";
+        }
+    }
+}
